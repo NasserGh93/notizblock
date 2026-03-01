dfd7d98316f37445ff97c0644bbf35477df269a7
NasserGh93 <nasserghaibeh@googlemail.com>
2026-03-01 15:10:09 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
+let notesTitle = ['ba', 'Aufgabe',];
 let notes = ['banana', 'rasen mähen', 'rasieren'];
+
+let trashNotes = []
 
 function renderNotes() {
     let contentRef = document.getElementById('content')
@@ -10,9 +13,24 @@ function renderNotes() {
     }
 
 }
-function getNoteTamplate(indexNote) {
-    return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>`;
+
+function renderTrashNotes() {
+    let trashcontentRef = document.getElementById('trash_content')
+    trashcontentRef.innerHTML = "";
+
+    for (let indexTrashNote = 0; indexTrashNote < notes.length; indexTrashNote++) {
+        trashcontentRef.innerHTML += getNoteTamplate(indexTrashNote);
+    }
+
 }
+function getNoteTamplate(indexNote) {
+    return `<p>+ title: ${notesTitle[indexNote]} -> <button onclick="deleteNote(${indexNote})">x</button></p>`;
+}
+
+function getTrashNoteTamplate(indexTrashNote) {
+    return `<p>+ ${trashNotes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>`;
+}
+
 function addNote() {
     let noteInputRef = document.getElementById('note_input');
     let noteInput = noteInputRef.value;
@@ -27,8 +45,10 @@ function addNote() {
 //wann muss die notitz gelöscht werden
 // anzeige updaten
 function deleteNote(indexNote) {
-    notes.splice(indexNote, 1);
+    let trashNote = notes.splice(indexNote, 1);
+    trashNotes.push(trashNote);
     renderNotes();
+    renderTrashNotes()
 
 }
 //Delete notes
