18c3cc5832157c48cfa52f23b233f08590a3fd6b
NasserGh93 <nasserghaibeh@googlemail.com>
2026-03-01 13:40:47 +0100

    git initial

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let notes = ['banana', 'rasen mähen'];
+// -> when are they displayed
+function renderNotes() {
+    let contentRef = document.getElementById('content')
+    contentRef.innerHTML = notes;
+
+}
+
+
+
+
+
+//Add notes
+
+//Delete notes
+//Archive notes
