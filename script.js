58fa971936bc7c778b639cfe4a42d89bb712420d
NasserGh93 <nasserghaibeh@googlemail.com>
2026-03-01 14:24:07 +0100

    Notizen Hinzufügen ->inputfeld clearen nach der Eingabe

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 
-let notes = ['banana', 'rasen mähen'];
-// -> when are they displayed
+let notes = ['banana', 'rasen mähen', 'rasieren'];
+
 function renderNotes() {
     let contentRef = document.getElementById('content')
-    contentRef.innerHTML = notes;
+    contentRef.innerHTML = "";
 
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        const note = notes[indexNote];
+        contentRef.innerHTML += getNoteTamplate(note);
+    }
+
+}
+function getNoteTamplate(note) {
+    return `<p>+ ${note}</p>`;
+}
+//Add notes
+function addNote() {
+    let noteInputRef = document.getElementById('note_input');
+    let noteInput = noteInputRef.value;
+
+    notes.push(noteInput);
+    renderNotes();
+
+    noteInputRef.value = "";
 }
 
 
 
 
-
-//Add notes
 
 //Delete notes
 //Archive notes
